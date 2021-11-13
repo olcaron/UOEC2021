53153034b6cccef8b3128938f94588e0d94ee2ad
antoinelavigne04 <alavi091@uottawa.ca>
2021-11-13 11:54:12 -0500

    Adding high level files

---
+++ src/Coordinates.java
@@ -0,0 +1,11 @@
+public class Coordinates {
+    public double elevation;
+    public double latitude;
+    public double longitude;
+
+    public Coordinates(double elevation, double latitude, double longitude) {
+        this.elevation = elevation;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        
+    }
+
+    public Coordinates getCoordinatesWithAdress() {
+        return null;
+    }
+
+    public void findPath() {}
+
+    public void decoler() {}
+
+    public void atterir() {}
 }
